53ea58a65072b1f2316ed8011dd83cd30d8f7365
Damian <damianhaq@gmail.com>
2024-10-26 13:56:11 +0200

    add img import

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import { Game } from "./DGamev3.js";
 const game = new Game();
 game.init("canvas", 800, 600, 2);
 
+const bigSpritev7 = new Image();
+bigSpritev7.src = "BigSpritev7.png";
+
 // Game loop
 requestAnimationFrame(gameLoop);
 let lastTime = 0;
